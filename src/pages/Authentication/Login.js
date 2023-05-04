a975f39414e72f7e07c4039c560981a37561540b
k.javedan <javedankhaled@gmail.com>
2023-05-04 20:29:02 +0400

    Added login and register dummmy structure

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -1,4 +1,4 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
--- src/pages/Authentication/Register.js
+++ src/pages/Authentication/Register.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Button, TextField } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  //NAVIGATION
+  const navigat = useNavigate();
 
-export default Register
+  //FUNCS
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigat("/home");
+  };
+  return (
+    <div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <TextField label="email" />
+        <TextField label="username" />
+        <TextField label="invitatino code" />
+        <TextField label="pass" />
+        <TextField label="confirm pass" />
+        <Button type="submit">register</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
